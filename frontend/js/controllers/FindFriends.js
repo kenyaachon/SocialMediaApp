72d92ca04bd4a3760970aad16f0fb9fdce07d54e
mosesmbugua <swahili1963@gmail.com>
2021-09-21 17:26:30 -0700

    BACKEND API

--- frontend/js/controllers/FindFriends.js
+++ frontend/js/controllers/FindFriends.js
@@ -1,5 +1,45 @@
 //var Ractive = require("ractive");
 
+var Friends = require("../models/Friends");
+var renderFindFriends = function (model) {
+  var self = this;
+
+  this.on("find", function (e) {
+    self.set("loading", true);
+    self.set("message", "");
+    var searchFor = this.get("friendName");
+    model.find(searchFor, function (err, res) {
+      if (res.friends && res.friends.length > 0) {
+        self.set("foundFriends", res.friends);
+      } else {
+        self.set("foundfriends", null);
+        self.set(
+          "message",
+          "Sorry, there is no friends matching <strong>" +
+            searchFor +
+            "<strong>"
+        );
+      }
+      self.set("loading", false);
+    });
+  });
+};
+
+var renderAddFriennds = function (model) {
+  this.on("add", function (e, id) {
+    this.set("loading", true);
+    model.add(id, function (err, res) {
+      self.set("foundFriends", null);
+      if (err) {
+        self.set("message", "Operation failed");
+      } else if (res.success === "OK") {
+        self.set("message", "Operation successful");
+      }
+      self.set("loading", false);
+    });
+  });
+};
+
 module.exports = Ractive.extend({
   template: require("../../tpl/find-friends"),
   components: {
@@ -14,5 +54,8 @@ module.exports = Ractive.extend({
   },
   onrender: function () {
     //rendering
+    var model = new Friends();
+    renderFindFriends.call(this, model);
+    renderAddFriennds.call(this, model);
   },
 });
--- frontend/js/controllers/Register.js
+++ frontend/js/controllers/Register.js
@@ -3,11 +3,14 @@ var UserModel = require("../models/User");
 var userModel = new UserModel();
 var registrationRender = function () {
   var self = this;
+  console.log("Why is this not fucking working!!!");
   this.observe("fistName", userModel.setter("value.firstName"));
   this.observe("lastName", userModel.setter("value.lastName"));
   this.observe("email", userModel.setter("value.email"));
   this.observe("password", userModel.setter("value.password"));
-  this.on("register", function () {
+
+  this.on("register", function (event) {
+    console.log("User just clicked the button, and now registrating them");
     userModel.create(function (error, result) {
       if (error) {
         self.set("error", error.error);
@@ -21,6 +24,7 @@ var registrationRender = function () {
     });
   });
 };
+
 module.exports = Ractive.extend({
   template: require("../../tpl/register"),
   components: {
@@ -28,7 +32,40 @@ module.exports = Ractive.extend({
     appfooter: require("../views/Footer"),
   },
   onrender: function () {
-    console.log("rendering registration");
+    console.log("rendering registration in the controller");
     registrationRender.call(this);
   },
 });
+// var ractive = Ractive.extend({
+//   template: require("../../tpl/register"),
+//   components: {
+//     navigation: require("../views/Navigation"),
+//     appfooter: require("../views/Footer"),
+//   },
+//   onrender: function () {
+//     var self = this;
+//     console.log("Why is not fucking working!!!");
+//     this.observe("fistName", userModel.setter("value.firstName"));
+//     this.observe("lastName", userModel.setter("value.lastName"));
+//     this.observe("email", userModel.setter("value.email"));
+//     this.observe("password", userModel.setter("value.password"));
+//   },
+// });
+
+// ractive.on("register", function () {
+//   var self = this;
+//   console.log("User just clicked the button, and now registrating them");
+//   userModel.create(function (error, result) {
+//     if (error) {
+//       self.set("error", error.error);
+//     } else {
+//       self.set("error", false);
+//       self.set(
+//         "success",
+//         'Registration successful. Click <a href="/login">here</a> to LEGAL_TCP_SOCKET_OPTIONS.'
+//       );
+//     }
+//   });
+// });
+
+// module.exports = ractive;
